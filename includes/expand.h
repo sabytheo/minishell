1af3aa5fa13e47448a8e44a2d394d3205a3eae1a
Teatime <tealeafdrink@proton.me>
2025-05-27 12:54:45 +0200

    "merge conflicts"

--- includes/expand.h
+++ includes/expand.h
@@ -6,13 +6,14 @@
 /*   By: egache <egache@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 16:34:58 by tsaby             #+#    #+#             */
-/*   Updated: 2025/05/06 18:41:52 by egache           ###   ########.fr       */
+/*   Updated: 2025/05/27 12:51:04 by egache           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EXPAND_H
 # define EXPAND_H
 
+# include "minishell.h"
 # include <stdbool.h>
 
 typedef struct s_expand

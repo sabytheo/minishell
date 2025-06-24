d6b19bd27365104c142522511165f4a25f1a1f48
Pioupiou <saby.theo43@gmail.com>
2025-06-24 12:35:37 +0200

    some changer, one is about cd

--- sources/builtins/cd.c
+++ sources/builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: egache <egache@student.42lyon.fr>          +#+  +:+       +#+        */
+/*   By: tsaby <tsaby@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/09 12:43:26 by egache            #+#    #+#             */
-/*   Updated: 2025/06/19 17:09:27 by egache           ###   ########.fr       */
+/*   Updated: 2025/06/24 10:17:36 by tsaby            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,21 +32,23 @@ int	ft_cd(t_minishell *minishell, t_cmds **cmds)
 {
 	int		cd;
 
-	if ((*cmds)->args[2] != NULL)
+	if ((*cmds)->args[1] == NULL)
+		cd = chdir("/home");
+	else if ((*cmds)->args[2] != NULL)
 	{
 		ft_printf_fd(2, "minishell: cd: too many arguments\n");
 		return (1);
 	}
-	if ((*cmds)->args[1] == NULL)
-		cd = chdir("/home");
 	else
-		cd = chdir((*cmds)->args[1]);
-	update_pwd(minishell);
-	if (cd != 0)
 	{
-		ft_printf_fd(2, "minishell: cd: %s: ", (*cmds)->args[1]);
-		perror(NULL);
-		return (1);
+		cd = chdir((*cmds)->args[1]);
+		update_pwd(minishell);
+		if (cd != 0)
+		{
+			ft_printf_fd(2, "minishell: cd: %s: ", (*cmds)->args[1]);
+			perror(NULL);
+			return (1);
+		}
 	}
 	return (0);
 }
